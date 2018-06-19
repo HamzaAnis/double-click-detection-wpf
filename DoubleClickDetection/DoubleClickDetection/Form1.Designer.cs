327552a5512864dfac584c4273b45608d27e2f63
Hamza Anis <hamzaanis9514@gmail.com>
2018-06-20 01:09:17 +0500

    Detectoin done

--- DoubleClickDetection/DoubleClickDetection/Form1.Designer.cs
+++ DoubleClickDetection/DoubleClickDetection/Form1.Designer.cs
@@ -29,6 +29,13 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.startToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.stopToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hideToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.showToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
@@ -41,23 +48,93 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click_1);
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(142, 109);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(119, 46);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Start detecting";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.startToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(415, 28);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // startToolStripMenuItem
+            // 
+            this.startToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.stopToolStripMenuItem,
+            this.hideToolStripMenuItem,
+            this.showToolStripMenuItem});
+            this.startToolStripMenuItem.Name = "startToolStripMenuItem";
+            this.startToolStripMenuItem.Size = new System.Drawing.Size(56, 24);
+            this.startToolStripMenuItem.Text = "Tools";
+            // 
+            // stopToolStripMenuItem
+            // 
+            this.stopToolStripMenuItem.Name = "stopToolStripMenuItem";
+            this.stopToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)(((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Alt) 
+            | System.Windows.Forms.Keys.X)));
+            this.stopToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.stopToolStripMenuItem.Text = "Stop";
+            this.stopToolStripMenuItem.Click += new System.EventHandler(this.stopToolStripMenuItem_Click);
+            // 
+            // hideToolStripMenuItem
+            // 
+            this.hideToolStripMenuItem.Name = "hideToolStripMenuItem";
+            this.hideToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)(((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Alt) 
+            | System.Windows.Forms.Keys.H)));
+            this.hideToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.hideToolStripMenuItem.Text = "Hide";
+            this.hideToolStripMenuItem.Click += new System.EventHandler(this.hideToolStripMenuItem_Click);
+            // 
+            // showToolStripMenuItem
+            // 
+            this.showToolStripMenuItem.Name = "showToolStripMenuItem";
+            this.showToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)(((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Alt) 
+            | System.Windows.Forms.Keys.S)));
+            this.showToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.showToolStripMenuItem.Text = "Show";
+            this.showToolStripMenuItem.Click += new System.EventHandler(this.showToolStripMenuItem_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(415, 406);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Form1";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem startToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem stopToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem hideToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem showToolStripMenuItem;
     }
 }
 
--- DoubleClickDetection/DoubleClickDetection/Form1.cs
+++ DoubleClickDetection/DoubleClickDetection/Form1.cs
@@ -18,7 +18,8 @@ namespace DoubleClickDetection
         DateTime clickedTime;
         bool IsSecondClick = false;
         System.Diagnostics.Stopwatch watch;
-
+        Rectangle rectangle;
+        bool isStart = false;
         [DllImport("user32.dll")]
         static extern uint GetDoubleClickTime();
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
@@ -40,7 +41,7 @@ namespace DoubleClickDetection
             mh.MouseClickEvent += mh_MouseClickEvent;
             mh.MouseDownEvent += mh_MouseDownEvent;
             mh.MouseUpEvent += mh_MouseUpEvent;
-
+            rectangle = Rectangle.Empty;
         }
         private void mh_MouseDownEvent(object sender, MouseEventArgs e)
         {
@@ -49,12 +50,16 @@ namespace DoubleClickDetection
                 watch.Stop();
                 Int64 elapsedTime = watch.ElapsedMilliseconds;
                 UInt32 doubleClickTime = GetDoubleClickTime();
-                Console.Out.WriteLine("Elapsed time is: " + elapsedTime);
-                Console.Out.WriteLine("Double Click time is " + GetDoubleClickTime().ToString());
                 IsSecondClick = false;
-                if ((e.Location.Y - mouseVerticalPosition == 0) & e.Location.X - mouseHorizontalPosition == 0 & elapsedTime<doubleClickTime)
+                if ((e.Location.Y - mouseVerticalPosition == 0) & e.Location.X - mouseHorizontalPosition == 0 & elapsedTime < doubleClickTime)
                 {
-                    Console.Out.WriteLine("Double Click Detected");
+                    if (isStart)
+                    {
+                        if (e.Location.X<=rectangle.Location.X+rectangle.Width&& e.Location.Y <= rectangle.Location.Y + rectangle.Height)
+                        {
+                            MessageBox.Show("It is pressed inside the area");
+                        }
+                    }
                 }
             }
             else
@@ -67,7 +72,7 @@ namespace DoubleClickDetection
 
 
             }
-
+            /*
             if (e.Button == MouseButtons.Left)
             {
                 Console.Out.WriteLine("Left Button Press\n");
@@ -75,12 +80,12 @@ namespace DoubleClickDetection
             if (e.Button == MouseButtons.Right)
             {
                 Console.Out.WriteLine("Right Button Press\n");
-            }
+            }*/
         }
 
         private void mh_MouseUpEvent(object sender, MouseEventArgs e)
         {
-
+            /*
             if (e.Button == MouseButtons.Left)
             {
                 Console.Out.WriteLine("Left Button Release\n");
@@ -88,21 +93,21 @@ namespace DoubleClickDetection
             if (e.Button == MouseButtons.Right)
             {
                 Console.Out.WriteLine("Right Button Release\n");
-            }
+            }*/
 
         }
         private void mh_MouseClickEvent(object sender, MouseEventArgs e)
         {
+            /*
             if (e.Button == MouseButtons.Left)
             {
-                string sText = "(" + e.X.ToString() + "," + e.Y.ToString() +","+e.Clicks+ ")";
-                //Console.Out.WriteLine("Double Click time is "+ GetDoubleClickTime().ToString());
+                string sText = "(" + e.X.ToString() + "," + e.Y.ToString() + "," + e.Clicks + ")";
             }
             if (e.Button == MouseButtons.Right)
             {
                 string sText = "(" + e.X.ToString() + "," + e.Y.ToString() + "," + e.Clicks + ")";
                 Console.Out.WriteLine(sText);
-            }
+            }*/
         }
 
         private void mh_MouseMoveEvent(object sender, MouseEventArgs e)
@@ -118,7 +123,7 @@ namespace DoubleClickDetection
         }
         protected override void OnMouseDown(MouseEventArgs e)
         {
-           
+
         }
 
         private void locationSelectToolStripMenuItem_Click(object sender, EventArgs e)
@@ -131,8 +136,139 @@ namespace DoubleClickDetection
 
         private void button1_Click_1(object sender, EventArgs e)
         {
-                        var bmp = SnippingTool.Snip();
+             rectangle = SnippingTool.Snip();
 
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (rectangle == Rectangle.Empty)
+            {
+                MessageBox.Show("Please select the area to detect");
+            }
+            else
+            {
+                /*
+                Console.Out.WriteLine("Width is: " + rectangle.Width);
+                Console.Out.WriteLine("Height is: " + rectangle.Height);
+                Console.Out.WriteLine("Location  is: " + rectangle.Location.ToString());
+                Console.Out.WriteLine("Location y is: " + rectangle.Location.Y);
+                Console.Out.WriteLine("Location x is: " + rectangle.Location.X);
+                */
+                MessageBox.Show("Detection started");
+                isStart = true;
+            }
+        }
+
+        private void showToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Console.Out.WriteLine("Show this");
+        }
+
+        private void stopToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            isStart = false;
+        }
+
+        private void hideToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Console.Out.WriteLine("Hiding this");
+        }
+    }
+
+    public partial class SnippingTool : Form
+    {
+        public static Rectangle Snip()
+        {
+            var rc = Screen.PrimaryScreen.Bounds;
+            using (Bitmap bmp = new Bitmap(rc.Width, rc.Height, System.Drawing.Imaging.PixelFormat.Format32bppPArgb))
+            {
+                using (Graphics gr = Graphics.FromImage(bmp))
+                    gr.CopyFromScreen(0, 0, 0, 0, bmp.Size);
+                using (var snipper = new SnippingTool(bmp))
+                {
+                    if (snipper.ShowDialog() == DialogResult.OK)
+                    {
+                        return snipper.rcSelect;
+                    }
+                }
+                return Rectangle.Empty;
+            }
+        }
+
+        public SnippingTool(Image screenShot)
+        {
+            this.BackgroundImage = screenShot;
+            this.ShowInTaskbar = false;
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.WindowState = FormWindowState.Maximized;
+            this.DoubleBuffered = true;
+        }
+        public Image Image { get; set; }
+
+        private Rectangle rcSelect = new Rectangle();
+        private Point pntStart;
+
+        protected override void OnMouseDown(MouseEventArgs e)
+        {
+
+
+            // Start the snip on mouse down
+            if (e.Button != MouseButtons.Left) return;
+            pntStart = e.Location;
+            rcSelect = new Rectangle(e.Location, new Size(0, 0));
+            this.Invalidate();
+        }
+        protected override void OnMouseMove(MouseEventArgs e)
+        {
+            // Modify the selection on mouse move
+            if (e.Button != MouseButtons.Left) return;
+            int x1 = Math.Min(e.X, pntStart.X);
+            int y1 = Math.Min(e.Y, pntStart.Y);
+            int x2 = Math.Max(e.X, pntStart.X);
+            int y2 = Math.Max(e.Y, pntStart.Y);
+            rcSelect = new Rectangle(x1, y1, x2 - x1, y2 - y1);
+
+/*            Console.Out.WriteLine("Width is: " + rcSelect.Width);
+            Console.Out.WriteLine("Height is: " + rcSelect.Height);
+            Console.Out.WriteLine("Location is: " + rcSelect.Location.ToString());
+*/
+            this.Invalidate();
+        }
+        protected override void OnMouseUp(MouseEventArgs e)
+        {
+            // Complete the snip on mouse-up
+            if (rcSelect.Width <= 0 || rcSelect.Height <= 0) return;
+            Image = new Bitmap(rcSelect.Width, rcSelect.Height);
+            using (Graphics gr = Graphics.FromImage(Image))
+            {
+                gr.DrawImage(this.BackgroundImage, new Rectangle(0, 0, Image.Width, Image.Height),
+                    rcSelect, GraphicsUnit.Pixel);
+            }
+            DialogResult = DialogResult.OK;
+        }
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            // Draw the current selection
+            using (Brush br = new SolidBrush(Color.FromArgb(120, Color.White)))
+            {
+                int x1 = rcSelect.X; int x2 = rcSelect.X + rcSelect.Width;
+                int y1 = rcSelect.Y; int y2 = rcSelect.Y + rcSelect.Height;
+                e.Graphics.FillRectangle(br, new Rectangle(0, 0, x1, this.Height));
+                e.Graphics.FillRectangle(br, new Rectangle(x2, 0, this.Width - x2, this.Height));
+                e.Graphics.FillRectangle(br, new Rectangle(x1, 0, x2 - x1, y1));
+                e.Graphics.FillRectangle(br, new Rectangle(x1, y2, x2 - x1, this.Height - y2));
+            }
+            using (Pen pen = new Pen(Color.Red, 3))
+            {
+                e.Graphics.DrawRectangle(pen, rcSelect);
+            }
+        }
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            // Allow canceling the snip with the Escape key
+            if (keyData == Keys.Escape) this.DialogResult = DialogResult.Cancel;
+            return base.ProcessCmdKey(ref msg, keyData);
         }
     }
 
